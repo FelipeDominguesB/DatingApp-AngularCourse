8ea750e3e55175bdedb7d1e682a2e3f3158a26e5
FelipeDominguesB <felipedom2001@hotmail.com>
2022-06-19 18:13:04 -0300

    Commit inicial, final da sessão 2

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Controllers;
+using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            if(users.Count > 0)
+            {
+                return users;
+
+            }
+
+            return null;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUserById(int id)
+        {
+            var user = await _context.Users.Where(x => x.Id == id).FirstOrDefaultAsync();
+            return user;
+        }
+    }
+}
