bae98ebd1bf7ae1f793a0df0d76f4b1a8d8e9e15
FelipeDominguesB <felipedom2001@hotmail.com>
2022-06-22 16:53:35 -0300

    Modfiiquei a pagina inicial do projeto angular, adicionei permissionamento pro cors no .net

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+    title = 'The Dating App';
+
+    users: any;
+    constructor(private http: HttpClient) {
+
+    }
+
+    ngOnInit(): void {
+        this.getUsers();
+
+    }
+
+    getUsers()
+    {
+        this.http.get("https://localhost:5001/api/user").subscribe(res =>{
+            this.users = res;
+        }, err =>{
+            console.log(err);
+        });
+    }
 }
